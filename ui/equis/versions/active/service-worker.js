9de4bb2e498258896f70b7409dec072882748449
clionaderoiste <cliona.deroiste@hmhco.com>
2019-11-14 14:33:24 +0000

    fixing paths in active version

--- ui/equis/versions/active/service-worker.js
+++ ui/equis/versions/active/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.420e83c26700fe4b0569a13cbca6269d.js"
+  "./precache-manifest.a4aa55148f391b93a17f008629552231.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
