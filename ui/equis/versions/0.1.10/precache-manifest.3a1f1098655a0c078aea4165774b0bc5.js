f28dfc201ddcfc81fc5fd3057e5af14327029de8
clionaderoiste <cliona.deroiste@hmhco.com>
2019-11-14 17:25:18 +0000

    adding bob build

---
+++ ui/equis/versions/0.1.10/precache-manifest.3a1f1098655a0c078aea4165774b0bc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8f59f96430e6adec2e3cbdb197ac7d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5b333064d5b7b18954a",
+    "url": "./static/css/main.5e376ef0.chunk.css"
+  },
+  {
+    "revision": "578bb8b7351628532bb7",
+    "url": "./static/js/2.f3573822.chunk.js"
+  },
+  {
+    "revision": "f5b333064d5b7b18954a",
+    "url": "./static/js/main.950545e1.chunk.js"
+  },
+  {
+    "revision": "489b4b9927008a51448e",
+    "url": "./static/js/runtime-main.410f99cd.js"
+  },
+  {
+    "revision": "298ff38d17fc13b57cc6f9f115c525e7",
+    "url": "./static/media/DosEquis.298ff38d.jpeg"
+  }
+]);
