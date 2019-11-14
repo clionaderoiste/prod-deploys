96fa26ca41c5b9446ec71658665100c90a57cbb0
wadhwas <shailesh.wadhwa@hmhco.com>
2019-11-14 15:04:25 +0000

    modified the UI

--- ui/login/users-list/js/scripts.js
+++ ui/login/users-list/js/scripts.js
@@ -5,7 +5,7 @@ console.log('success');
 
 $.getJSON('js/data.json', function(data){
     $.each(data.users, function(key, val) {
-       $(list).append('<div class="col-xs-6 col-sm-6 col-md-3"><div class="thumbnail"><img src="'+val.avatar+'" height="72" width="72" alt="http://placehold.it/72x72"><div class="caption"><h4>'+val.username+'</h4><p align="justify">'+val.about+'</p><div><a href="'+val.link+'" class="btn btn-info btn-xs" role="button">ENTER</a></div></div></div></div>');
+       $(list).append('<div class="col-xs-6 col-sm-6 col-md-3"><div class="thumbnail"><img class="img-thumbnail" src="'+val.avatar+'" height="72" width="72" alt="http://placehold.it/72x72"><div class="caption"><h4>'+val.username+'</h4><p align="justify">'+val.about+'</p><div></div><a href="'+val.link+'" class="btn btn-primary btn-xs" role="button">ENTER</a></div></div></div>');
     })
 });
 });
