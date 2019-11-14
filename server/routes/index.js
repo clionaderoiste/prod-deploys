cdb373dd9124fa790596aec0e7ab5dccd738f47c
clionaderoiste <cliona.deroiste@hmhco.com>
2019-11-14 14:14:07 +0000

    changing static and proxy rather than route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,14 +5,15 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function(req, res, next) {
   let version = req.query.version || "active";
-  res.sendFile(
-    path.join(__dirname, "../../ui/equis/versions/" + version + "/index.html"),
-    function(err) {
-      res.sendFile(
-        path.join(__dirname, "../../ui/equis/versions/active/index.html")
-      );
-    }
-  );
+  res.redirect('/'+version);
+  // res.sendFile(
+  //   path.join(__dirname, "../../ui/equis/versions/" + version + "/index.html"),
+  //   function(err) {
+  //     res.sendFile(
+  //       path.join(__dirname, "../../ui/equis/versions/active/index.html")
+  //     );
+  //   }
+  // );
 });
 
 module.exports = router;
