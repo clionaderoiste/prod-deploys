b756d227de5816ba5829665d4c87332bd97376d6
clionaderoiste <cliona.deroiste@hmhco.com>
2019-11-14 16:41:03 +0000

    beta version

--- ui/equis/src/App.js
+++ ui/equis/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './images/DosEquis.jpeg';
+import beta from './images/Beta.png';
 import './App.css';
 
 function App() {
@@ -7,7 +8,10 @@ function App() {
     <div className="App">
       <header className="App-header gridLayout">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-version">This is application version {process.env.REACT_APP_VERSION}</p>
+        <div>
+          <img src={beta} className="App-beta" alt="beta logo" />
+          <p className="App-version">This is application version {process.env.REACT_APP_VERSION}</p>
+        </div>
       </header>
     </div>
   );
