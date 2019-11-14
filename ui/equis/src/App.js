69adc67b8d9c9e88693829715583589c7a2956ca
clionaderoiste <cliona.deroiste@hmhco.com>
2019-11-14 13:22:42 +0000

    adding equis logo

--- ui/equis/src/App.js
+++ ui/equis/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/DosEquis.jpeg';
 import './App.css';
 
 function App() {
@@ -7,9 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <p>This is application version {process.env.REACT_APP_VERSION}</p>
       </header>
     </div>
