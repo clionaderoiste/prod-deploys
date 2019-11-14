9f8539dc85bd834b6818fd81dba15dff8b59134e
clionaderoiste <cliona.deroiste@hmhco.com>
2019-11-14 17:10:38 +0000

    adding a version with a squashed bug

--- ui/equis/src/App.js
+++ ui/equis/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './images/DosEquis.jpeg';
+import bug from './images/Squashed bug.jpeg'
 import './App.css';
 
 function App() {
@@ -7,7 +8,10 @@ function App() {
     <div className="App">
       <header className="App-header gridLayout">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-version">This is application version {process.env.REACT_APP_VERSION}</p>
+        <div>
+          <img src={bug} className="" alt="logo" />
+          <p className="App-version">This is application version {process.env.REACT_APP_VERSION}</p>
+        </div>
       </header>
     </div>
   );
